ac226a908df1069366f39fc03d34a1a18c3f83c1
ysatyam129 <yadavsatyamsingh078@gmail.com>
2025-09-08 03:27:45 +0530

    cors corrected

--- simple-server.js
+++ simple-server.js
@@ -16,7 +16,7 @@ const app = express();
 
 // Middleware
 app.use(cors({
-  origin: ['http://localhost:3000', 'http://127.0.0.1:3000'],
+  origin: ['http://localhost:3000', 'http://127.0.0.1:3000',process.env.FRONTEND_URL],
   credentials: true,
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
   allowedHeaders: ['Content-Type', 'Authorization']
